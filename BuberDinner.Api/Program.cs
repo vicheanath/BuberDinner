c9a36811a80b42d83f0c7e6a4b7b5e9d6d277e4b
Vichea <48352653+vicheanath@users.noreply.github.com>
2024-04-27 11:30:40 -0500

    Add glovle error handle

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,9 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -7,10 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    // app.Map("/error", (HttpContext context) =>
+    // {
+    //     Exception? exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+    //     return Results.Problem();
+    // });
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
