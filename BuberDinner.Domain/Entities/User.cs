d1479529d022d52582beebc1dc23d5edf2b61544
Vichea <48352653+vicheanath@users.noreply.github.com>
2024-05-13 00:56:56 -0500

    Add doc

--- BuberDinner.Domain/Entities/User.cs
+++ BuberDinner.Domain/Entities/User.cs
@@ -1,6 +1,6 @@
 namespace BuberDinner.Domain.Entities;
 
-public class User : Entity<Guid>
+public class User
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string FirstName { get; set; } = null!;
